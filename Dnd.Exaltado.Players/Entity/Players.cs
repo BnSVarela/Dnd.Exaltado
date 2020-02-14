52df3d27daec089a24169dd14e1e0cb5d1400738
Bruno Varela <bnsvarela@gmail.com>
2020-02-14 16:10:02 -0300

    Limpeza dos testes

--- Dnd.Exaltado.Players/Entity/Players.cs
+++ Dnd.Exaltado.Players/Entity/Players.cs
@@ -17,47 +17,5 @@ namespace Dnd.Exaltado.Players.Entity
         public string CharacterName { get; set; }
         [DataMember(Name = "_Id")]
         public int _Id { get; set; }
-        [DataMember(Name = "_XX")]
-        public bool _XX { get; set; }
-        [DataMember(Name = "_IdX")]
-        public int _IdX { get; set; }
-        [DataMember(Name = "_DateTeste")]
-        public DateTime _DateTeste { get; set; }
-        [DataMember(Name = "_PlayersTeste")]
-        public PlayersTeste _PlayersTeste { get; set; }
-        [DataMember(Name = "PlayerName1")]
-        public string PlayerName1 { get; set; }
-        [DataMember(Name = "PlayerName2")]
-        public string PlayerName2 { get; set; }
-        [DataMember(Name = "PlayerName3")]
-        public string PlayerName3 { get; set; }
-        [DataMember(Name = "PlayerName4")]
-        public string PlayerName4 { get; set; }
-        [DataMember(Name = "PlayerName5")]
-        public string PlayerName5 { get; set; }
-        [DataMember(Name = "PlayerName6")]
-        public string PlayerName6 { get; set; }
-        [DataMember(Name = "PlayerName7")]
-        public string PlayerName7 { get; set; }
-        [DataMember(Name = "_PlayersTeste2")]
-        public PlayersTeste2 _PlayersTeste2 { get; set; }
-    }
-
-    [DataContract]
-    public class PlayersTeste
-    {
-        [DataMember(Name = "PlayerTesteInfo")]
-        public string PlayerTesteInfo { get; set; }
-        [DataMember(Name = "PlayerTesteInfo2")]
-        public string PlayerTesteInfo2 { get; set; }
-    }
-
-    [DataContract]
-    public class PlayersTeste2
-    {
-        [DataMember(Name = "PlayerTesteInfo")]
-        public string PlayerTesteInfo { get; set; }
-        [DataMember(Name = "PlayerTesteInfo2")]
-        public string PlayerTesteInfo2 { get; set; }
-    }
+    }    
 }
