be64a687a5a0c18ae0d350700a9cecc30254fcbc
Bruno Varela <bnsvarela@gmail.com>
2020-02-24 20:10:25 -0300

    Inicio do desenv de combate

---
+++ Dnd.Exaltado.Web/wwwroot/js/Combat.js
@@ -0,0 +1,18 @@
+﻿$(function () {
+
+    $('#tabela-xyz').DataTable({
+        "paging": false,
+        "lengthChange": false,
+        "searching": false,
+        "ordering": false,
+        "info": false,
+        "scrollY":"37.5vh"
+    });
+
+
+    $(document).on('click', '#go-to-next-turn', function (e) {
+        e.preventDefault();
+
+        $('#turn-value').text('8');
+    });
+});
